38ee51bd18c9cc932c375822e69db6379194b155
Halia Litai Lopez Alvarez <123999262+litailopez@users.noreply.github.com>
2024-06-03 23:20:56 -0600

    Delete app directory

--- app/calculadora/mac/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import  funcion_hola, funcion_calculadora
-
-urlpatterns = [
-    path('hola/', funcion_hola),
-    path('calculadora/', funcion_calculadora),
-]
--- app/calculadora/mac/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def funcion_hola(request):
-    return render(request, "hola.html")
-
-def funcion_calculadora(request):
-    return render(request, "calculadora.html")
